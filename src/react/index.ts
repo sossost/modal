651679e223fe2c524328e7f68f88403fe7888c4d
jang_yoonsu <shagrat8546@gmail.com>
2024-06-02 13:40:20 +0900

    feat : add index.ts of react folder

---
+++ src/react/index.ts
@@ -0,0 +1,5 @@
+import ModalContainer from "./ModalContainer";
+import ModalProvider from "./ModalProvider";
+import useModal from "./useModal";
+
+export { ModalContainer, ModalProvider, useModal };
