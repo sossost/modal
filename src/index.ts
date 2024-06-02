d92d6bc0735286ca2d13d84849b463afd3e982d2
jang_yoonsu <shagrat8546@gmail.com>
2024-06-02 13:41:47 +0900

    feat : add index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import ModalManager from "./ModalManager";
+import { ModalProvider, useModal } from "./react";
+import type { ModalProps } from "./types";
+
+export { ModalManager, ModalProvider, useModal };
+export type { ModalProps };
