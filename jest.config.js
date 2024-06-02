bca81b6d4ae5cf0531d879edfd5a8d25a44eb6cc
jang_yoonsu <shagrat8546@gmail.com>
2024-06-02 13:38:02 +0900

    chore : add jest config, setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: [
+    "**/tests/**/*.test.ts",
+    "**/tests/**/*.test.tsx",
+    "**/src/*.spec.ts",
+  ],
+};
